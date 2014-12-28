6b735d8529d4e2d5f541731079d9ad87834a1d1a
tarma <tarma_fu@hotmail.com>
2014-12-28 14:45:27 +0800

    Add hint when there isn't any news

--- src/com/edu/thss/smartdental/BBSInTabNewsFragment.java
+++ src/com/edu/thss/smartdental/BBSInTabNewsFragment.java
@@ -1,3 +1,6 @@
+/**
+ * Author: FU CHIN SENG
+ */
 package com.edu.thss.smartdental;
 
 import java.util.ArrayList;
@@ -15,12 +18,14 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class BBSInTabNewsFragment extends Fragment {
 	
 	private ListView listView;
 	private NewsListAdapter listAdapter;
 	private ArrayList<NewsElement> newsList;
+	private TextView noNewsText;
 	
 	public BBSInTabNewsFragment(ArrayList<NewsElement> newsList) {
 		this.newsList = newsList;
@@ -31,6 +36,10 @@ public class BBSInTabNewsFragment extends Fragment {
 		View rootView = inflater.inflate(R.layout.fragment_bbs_in_news, container, false);
 		
 		this.listView = (ListView) rootView.findViewById(R.id.bbs_news_list);
+		this.noNewsText = (TextView) rootView.findViewById(R.id.bbs_no_news_text);
+		if (newsList.size() > 0) {
+			noNewsText.setVisibility(View.INVISIBLE);
+		}
 		this.listAdapter = new NewsListAdapter(newsList, getActivity());
 		listView.setAdapter(listAdapter);
 		listView.setOnItemClickListener(new OnNewsListItemClickListener());
@@ -51,6 +60,11 @@ public class BBSInTabNewsFragment extends Fragment {
 			intent.putExtra("postId", element.getPostID());
 			startActivity(intent);
 			newsList.remove(position);
+			if (newsList.size() == 0) {
+				noNewsText.setVisibility(View.VISIBLE);
+			} else {
+				noNewsText.setVisibility(View.INVISIBLE);
+			}
 			listAdapter.notifyDataSetChanged();
 		}
 		
